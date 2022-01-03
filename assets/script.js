fa494e9a9a2488fe0e1b97d9a28cf0ac0407095e
Jon Chirinos <chirinosjon@gmail.com>
2022-01-03 02:01:26 -0600

    updated javascript

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
 let generateBtn = document.querySelector("#generate");
 // 1b. create array of letters
 let lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z"];
+let ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let pwNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let pwSymbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "?", "/", "+", "-"];
 // generatePassword
 // return a final password as a string
 function generatePassword() {
     // 1. assign a variable to the length of the password
-    let pwLength = prompt("how long has this been going on?");
+    let pwLength = prompt("Select Password Length Between 8-126 characters");
     let password = [];
     // 1a. create a password with that length
+    if (pwLength < 8 || pwLength > 126) {
+        alert("Please meet selected criteria");
+    } else {
+        let useLower = confirm("Would you like lowercase letters included?");
+    }
+    if (useLower === true) {
+        pwBank = pwBank.concat(lcLetters);
+    }
     // 1c. loop through array of letters
     for (let i = 0; i < pwLength; i++) {
         // 1d. push each letter array into password array
