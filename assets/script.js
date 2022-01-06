af95b50d186ac69dd2876b63633fdb199a8f763d
Jon Chirinos <chirinosjon@gmail.com>
2022-01-05 21:02:24 -0600

    finish javascript logic for password generatorn

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,77 @@
+// link logic to button
 let generateBtn = document.querySelector("#generate");
 // array of all available password characters
-let lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "x", "t", "u", "v", "w", "x", "y", "z"];
-let ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let pwNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let pwSymbol = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "?", "/", "+", "-"];
-// generatePassword
-// return a final password as a string
-// start of function
-function generatePassword() {
-    // 1. assign a variable to the length of the password
-    // asking for user input
-    let pwLength = prompt("Select Password Length Between 8-128 characters");
-    let password = [];
-    let choice = [];
-    // 1a. create a password with that length
-    if (pwLength < 8 || pwLength > 128) {
-        alert("Please meet selected criteria");
-        generatePassword();
-    } else {
-        // validated input from user
-        let useLower = confirm("Would you like lowercase letters included?");
-        let useUpper = confirm("Would you like uppercase letters included?");
-        let useNumber = confirm("Would you like numbers included?");
-        let useSymbol = confirm("Would you like symbols included?");
 
+// array of possible lowercase letters for password
+let lcLetters = ["abcdefghijklmnopqrstuvwxyz"];
+// array of possible uppercase letter for password
+let ucLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+// array of possible numbers letter for password
+let pwNumber = ["0123456789"];
+// array of possible symbols letter for password
+let pwSymbol = ["`!@#$%^&*?/<>}{)(=+-_"];
+
+// initialize generatePassword function
+function generatePassword() {
+    // establish password length
+    let pwLength = parseInt(prompt("Select Password Length Between 8-128 characters"));
+    // empty array to hold possible password characters
+    let possibleCharacters = [];
+    // if statement to ensure correct password length
+    if (pwLength < 8 || pwLength > 128) {
+        // generate alert
+        alert("Please meet selected criteria");
+        // continue if validation passed
+    } else {
+        // prompt asking user if they want to include lowercase letters
+        let useLower = confirm("Would you like lowercase letters included?");
+        // prompt asking user if they want to include uppercase letters
+        let useUpper = confirm("Would you like uppercase letters included?");
+        // prompt asking user if they want to include numbers letters
+        let useNumber = confirm("Would you like numbers included?");
+        // prompt asking user if they want to include symbols letters
+        let useSymbol = confirm("Would you like symbols included?");
+        // if user elects to include lowercase letters in password
         if (useLower === true) {
-            password = password.concat(lcLetters);
-            let randomItem = lcLetters;
+            // append lowercase letter to array
+            possibleCharacters = possibleCharacters.concat(lcLetters);
         }
+        // if user elects to include uppercase letters in password
         if (useUpper === true) {
-            password = password.concat(ucLetters);
+            // append uppercase letters to empty array
+            possibleCharacters = possibleCharacters.concat(ucLetters);
         }
+        // if user elects to include numbers letters in password
         if (useNumber === true) {
-            password = password.concat(useNumber);
+            // append numbers to array
+            possibleCharacters = possibleCharacters.concat(pwNumber);
         }
+        // if user elects to include symbols letters in password
         if (useSymbol === true) {
-            password = password.concat(useSymbol);
+            // append symbols to array
+            possibleCharacters = possibleCharacters.concat(pwSymbol);
         }
+        // combine possible character strings into one
+        let possibleCharactersString = possibleCharacters.join();
         // If user doesn't select any options
         if (useLower === false && useUpper == false && useNumber == false && useSymbol == false) {
+            // display alert
             alert("Your password must contain one of the aforementioned criteria");
-            generatePassword();
+            // continue if validation passed
         } else {
-            // 1c. loop through array of letters
-            for (let i = 0; i < pwLength; i++) {
-                let pickChoice = choice[Math.floor(Math.random() * choice.length)];
-                // 1d. push each letter array into password array
-                password.push(choice[i]);
+            // empty string to hold password
+            let pass = "";
+            // loop through length of password
+            for (var i = 0; i < pwLength; i++) {
+                // select random character from possibleCharactersString
+                var randomCharacter = Math.floor(Math.random() * possibleCharactersString.length);
+                // appending selected character above to password
+                pass += possibleCharactersString.substring(randomCharacter, randomCharacter + 1);
             }
+            // populate password in text box
+            document.getElementById("password").value = pass;
         }
     }
-    // 1e. create a random 10 letter password
-    // 2 ask user for length
-    // 1e. convert password array back to string
-    return password.join("");
 }
-// Write password to the #password input
-function writePassword() {
-    let password = generatePassword();
-    let passwordText = document.querySelector("#password");
-    passwordText.value = password;
-}
-// Add event listener to generate button
-// kicks off the generator
-generateBtn.addEventListener("click", writePassword);
-localStorage;
-choice;
+// event listener to initiate generatePassword function
+generateBtn.addEventListener("click", generatePassword);
